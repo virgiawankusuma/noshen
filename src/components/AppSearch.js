185a868ece5c2dfaa92e04d8486d452849a4cf80
virgiawankusuma <virgiawan150@gmail.com>
2022-06-21 12:57:15 +0700

    change statefull to stateless component

--- src/components/AppSearch.js
+++ src/components/AppSearch.js
@@ -1,26 +1,15 @@
 import React from 'react';
 
-class AppSearch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onSearchHandler = this.onSearchHandler.bind(this);
-  }
-
-  onSearchHandler({ target }) {
-    this.props.onSearch(target.value);
-  }
-
-  render() {
-    return (
-      <div className="noshen-search">
-        <input
-          type="search"
-          placeholder="Search.."
-          onChange={this.onSearchHandler}
-        />
-      </div>
-    );
-  }
+function AppSearch({ onSearch }) {
+  return (
+    <div className="noshen-search">
+      <input
+        type="search"
+        placeholder="Search.."
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default AppSearch;
